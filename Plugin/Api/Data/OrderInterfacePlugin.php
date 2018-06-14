34efb5e83ab834d94f967bca3e058152e2b3f2dc
Jeroen van Leusden <jeroen@reachdigital.nl>
2018-06-14 11:49:39 +0200

    Add Parcelshop Id as order extension attribute

---
+++ Plugin/Api/Data/OrderInterfacePlugin.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of the Magento 2 Shipping module of DPD Nederland B.V.
+ *
+ * Copyright (C) 2018  DPD Nederland B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace DPDBenelux\Shipping\Plugin\Api\Data;
+
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderExtensionInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+
+class OrderInterfacePlugin
+{
+    /** @var OrderExtensionFactory $extensionFactory */
+    private $extensionFactory;
+
+    /**
+     * @param OrderExtensionFactory $extensionFactory
+     */
+    public function __construct(OrderExtensionFactory $extensionFactory)
+    {
+        $this->extensionFactory = $extensionFactory;
+    }
+
+    /**
+     * @param OrderInterface               $entity
+     * @param OrderExtensionInterface|null $extensionAttributes
+     *
+     * @return OrderExtensionInterface
+     */
+    public function afterGetExtensionAttributes(
+        OrderInterface $entity,
+        OrderExtensionInterface $extensionAttributes = null
+    ): OrderExtensionInterface {
+        if ($extensionAttributes === null) {
+            $extensionAttributes = $this->extensionFactory->create();
+            $entity->setExtensionAttributes($extensionAttributes);
+        }
+
+        return $extensionAttributes;
+    }
+}
---
+++ Plugin/Api/OrderRepositoryInterfacePlugin.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the Magento 2 Shipping module of DPD Nederland B.V.
+ *
+ * Copyright (C) 2018  DPD Nederland B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace DPDBenelux\Shipping\Plugin\Api;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\ResourceModel\Order\Collection;
+
+class OrderRepositoryInterfacePlugin
+{
+    /**
+     * @param OrderRepositoryInterface $orderRepository
+     * @param Collection               $collection
+     *
+     * @return Collection
+     */
+    public function afterGetList(OrderRepositoryInterface $orderRepository, Collection $collection): Collection
+    {
+        foreach ($collection->getItems() as $order) {
+            /** @var OrderInterface $order */
+
+            $this->afterGet($orderRepository, $order);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * @param OrderRepositoryInterface $orderRepository
+     * @param OrderInterface           $entity
+     *
+     * @return OrderInterface
+     */
+    public function afterGet(OrderRepositoryInterface $orderRepository, OrderInterface $entity): OrderInterface
+    {
+        $extensionAttributes = $entity->getExtensionAttributes();
+        $extensionAttributes->setDpdParcelshopId($entity->getData('dpd_parcelshop_id'));
+        $entity->setExtensionAttributes($extensionAttributes);
+
+        return $entity;
+    }
+}
